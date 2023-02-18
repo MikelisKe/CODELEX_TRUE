ef1aed296ea0ef4e4a28cc77486b93a66d07e8ce
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-18 23:45:42 +0200

    Created variables using arrays.

--- csharp-basics/exercises/TypesAndVariables/VariablesAndNames/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/VariablesAndNames/Program.cs
@@ -9,21 +9,21 @@ namespace VariablesAndNames
             int cars, drivers, passengers, carsNotDriven, carsDriven;
             double seatsInCar, carpoolCapacity, averagePassengersPerCar;
 
-            cars = 100;                     
-            seatsInCar = 4;                
-            drivers = 28;                   
+            cars = 100;
+            seatsInCar = 4;
+            drivers = 28;
             passengers = 90;
             carsDriven = (int)(passengers / seatsInCar);
-            carsNotDriven = (int)(cars - carsDriven); 
-            carpoolCapacity = drivers*seatsInCar ; 
-            averagePassengersPerCar = (double)passengers/drivers; 
+            carsNotDriven = (int)(cars - carsDriven);
+            carpoolCapacity = drivers * seatsInCar;
+            averagePassengersPerCar = (double)passengers / drivers;
 
             Console.WriteLine("There are " + cars + " cars available.");
             Console.WriteLine("There are only " + drivers + " drivers available.");
             Console.WriteLine("There will be " + carsNotDriven + " empty cars today.");
             Console.WriteLine("We can transport " + carpoolCapacity + " people today.");
             Console.WriteLine("We have " + passengers + " to carpool today.");
-            Console.WriteLine("We need to put about " + Math.Round( averagePassengersPerCar, 2) + " in each car.");
+            Console.WriteLine("We need to put about " + Math.Round(averagePassengersPerCar, 2) + " in each car.");
             Console.ReadKey();
         }
     }
