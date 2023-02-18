3762ec20cf0ac4990419c8a8a4ce7c6aaf94a07f
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-18 23:56:54 +0200

    for cycle to find uppercase with Char.IsUpper

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise_7/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a word where we can count upper case: ");
+        string textToCountUppercase = Console.ReadLine();
+        int count = 0;
+        for (int i = 0; i < textToCountUppercase.Length; i++)
+        {
+            if (Char.IsUpper(textToCountUppercase[i]))
+                count++;
+        }
+        Console.WriteLine(count);
+        Console.ReadKey();
+    }
+}
