0145c3117709b98e90a91ac634b0cd3c8b1c13aa
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-19 00:01:51 +0200

    Used parse to convert readline to int, did the match to convert minutes to years and days.

--- csharp-basics/exercises/TypesAndVariables/Exercise_7/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise_7/Program.cs
@@ -5,11 +5,13 @@
         Console.WriteLine("Enter a word where we can count upper case: ");
         string textToCountUppercase = Console.ReadLine();
         int count = 0;
-        for (int i = 0; i < textToCountUppercase.Length; i++)
+
+        for (var i = 0; i < textToCountUppercase.Length; i++)
         {
             if (Char.IsUpper(textToCountUppercase[i]))
                 count++;
         }
+
         Console.WriteLine(count);
         Console.ReadKey();
     }
