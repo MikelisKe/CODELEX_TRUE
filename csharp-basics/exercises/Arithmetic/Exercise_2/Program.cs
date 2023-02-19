7ce08fa3de91499936fbb12fff3df4beddd7ef83
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-19 16:01:59 +0200

    created programm which creates odd number and every time loop to 10 creates odd number, it says "bye"

---
+++ csharp-basics/exercises/Arithmetic/Exercise_2/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CheckOddEven();
+
+        static void CheckOddEven()
+        {
+            for (var i = 1 ; i < 10; i++)
+            {
+                if (i%2 == 1)
+                {
+                    Console.WriteLine("bye");
+                }
+            }
+        }
+
+    }
+}
