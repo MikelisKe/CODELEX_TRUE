3cf3ed4932048937a6170f568c298d76679a9166
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-19 18:12:02 +0200

    Created program using for cycle ehre the variable factional was mutiplied with every num and again till it reached the value of num.

---
+++ csharp-basics/exercises/Arithmetic/Exercise_4/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 10;
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine("The factorial of {0} is {1}", num, factorial);
+
+    }
+}
