bf336cf79328813d5cffe0a52dbe3d41ab6b385c
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-20 18:52:04 +0200

    Created program that generates random number of 1 to 100 and am trying to guess it. Then says that im too high or too low with the guess

--- csharp-basics/exercises/Arithmetic/Exercise_5/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise_5/Program.cs
@@ -2,7 +2,21 @@
 {
     private static void Main(string[] args)
     {
-        Random num= new Random(100);
-        Console.WriteLine(num);
+        Console.WriteLine("I'm thinking of a number between 1-100.  Try to guess it.");
+        Random rnd = new Random();
+        int number = rnd.Next(1,100);
+        int guess = Convert.ToInt32(Console.ReadLine());
+        if (number == guess)
+        {
+            Console.WriteLine("You guessed it!  What are the odds?!?");
+        }
+        else if (number > guess)
+        {
+            Console.WriteLine($"Sorry, you are too low.  I was thinking of {number}");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, you are too high.  I was thinking of {number}");
+        }
     }
 }
