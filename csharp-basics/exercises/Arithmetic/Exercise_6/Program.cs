1a7a00ac77869428ad26c470a29ee3105bb7d9e8
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-20 19:46:38 +0200

    Izveidoju programmu izmantojot for loop un ik pēc 11 ieliku jaunu rindu ar writeline, pārējos ar write. Vērtības sareizināju, lai nav jāizmanto And(&&) operātors, jo šādi ir pārskatāmāk.

--- csharp-basics/exercises/Arithmetic/Exercise_6/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise_6/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+        private static void Main(string[] args)
+    {
+        const int max = 110;
+        CozaLozaWoza();
+        static void CozaLozaWoza()
+        {
+            for(var i =1; i< max; i++)
+            {                
+                if (i % 11 == 0)
+                {
+                    Console.WriteLine();
+                }
+                else if (i % 105 == 0)
+                {
+                    Console.Write("CozaLozaWoza ");
+                }                             
+                else if (i % 15 == 0 )
+                {
+                    Console.Write("CozaLoza ");
+                }
+                else if (i % 21 == 0)
+                {
+                    Console.Write("CozaWoza ");
+                }
+                else if (i % 35 == 0)
+                {
+                    Console.Write("WozaLoza ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Coza ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Loza ");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write("Woza ");
+                }
+                else
+                {
+                    Console.Write(i+ " ");
+                }
+            }                       
+        }
+    }
+}
