81a324ac4f07375f155ddad8a65697aa851bc138
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-21 21:30:08 +0200

    Created program to calculate BMI.

---
+++ csharp-basics/exercises/Arithmetic/Exercise_9/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Please input your weight");
+        int weight = int.Parse(Console.ReadLine());
+        Console.WriteLine("now height");
+        double height = double.Parse(Console.ReadLine());
+        double bmi = weight/ (height*height);
+
+        if (bmi <=18.5) 
+        {
+            Console.WriteLine($"Your bmi is {bmi} and you need to gain some weight");
+        }
+        else if(bmi > 18.5 && bmi < 25)
+        {
+            Console.WriteLine($"Your bmi is {bmi} and you`re ok");
+        }
+        else if (bmi > 25)
+        {
+            Console.WriteLine($"Your bmi is {bmi} you`re like me - fat");
+        }
+    }
+}
