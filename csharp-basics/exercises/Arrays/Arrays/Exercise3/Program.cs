f666ef0d11aa672dd02632023472dce7ffa18452
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-25 14:27:52 +0200

    Used for loop to sum the num of arrays and devided with length of num array

--- csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercise3
+﻿using System;
+
+namespace Exercise3
 {
     class Program
     {
@@ -6,19 +8,17 @@
         private static void Main(string[] args)
         {
             int[] numbers = {20, 30, 25, 35, -16, 60, -100};
-
-            /*
-            fixme - calculate sum of all array elements 
             int sum = 0;
-            for (?){
-                
-            }
-            */
 
-            /*
-            fixme - calculate average value 
+            for (int i = 0 ; i< numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            int average = sum / (numbers.Length);                     
             Console.WriteLine("Average value of the array elements is : " + average);
-            */
+            Console.ReadKey();
+            
         }
     }
 }
