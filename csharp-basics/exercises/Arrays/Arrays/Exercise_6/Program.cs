254aa39114f4bb5b580a8c0206a6f70f39b6e360
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-02-26 13:50:30 +0200

    Created array of 10 with random numbers. And copy with last digid -7

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise_6/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int[] arrayOfTen = new int[10];
+
+        for (int i = 0; i < arrayOfTen.Length; i++)
+        {
+            arrayOfTen[i] = rnd.Next(100);
+        }
+
+        for (int i = 0; i < arrayOfTen.Length; i++)
+        {
+            Console.Write(arrayOfTen[i] + " ");
+        }
+
+        Console.WriteLine( "\n");
+        int[] copyOfArray = arrayOfTen;
+        copyOfArray[9] = -7;
+
+        for (int i = 0; i < copyOfArray.Length; i++)
+        {
+            Console.Write(arrayOfTen[i] + " ");
+        }
+
+        Console.ReadKey();
+        }
+}
