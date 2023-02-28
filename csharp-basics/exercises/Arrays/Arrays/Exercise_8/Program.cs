c14f66fd92b03a2d2a2085b18bfc362949262bd0
Miķelis Keišs <mikelis.keiss90@gmail.com>
2023-02-28 13:33:03 +0200

    Programm works. With some innovative missed letters, but it works.

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise_8/Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {         
+        string[] words = { "fanta", "cola" };
+        
+        Random rnx = new Random();
+        int randNum = rnx.Next(words.Length);
+        string randomWord = words[randNum].ToLower();
+        Console.WriteLine(randomWord);
+        string underLine = "";
+        int missCount = randomWord.Length * randomWord.Length;
+        string misses = "";
+        int randcount = randomWord.Length;
+
+        Console.WriteLine("Welcome to Hangman");
+        Console.Write("Enter your first letter:");
+
+        for (int i = 0; i < randomWord.Length; i++)
+        {
+            underLine += "_";
+        }
+
+        while (missCount !=0 && randcount !=0)   
+        {
+            char guess = Console.ReadKey().KeyChar;
+
+            for (int i = 0 ; i < randomWord.Length ; i++ )
+            {
+                if (guess == randomWord[i])
+                {
+                    underLine = underLine.Substring(0, i) + guess.ToString() + underLine.Substring(i +1);
+                }
+                else if (guess != randomWord[i])
+                {
+                    missCount--;
+                    misses += guess;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("+-+-+-+-+-+-+-+-+-+-+-+-");
+            Console.WriteLine();
+            Console.WriteLine($"word: {underLine}");
+            Console.WriteLine();
+            Console.WriteLine($"Misses : {misses}");
+            Console.WriteLine();
+            Console.WriteLine($"Guess :  {guess}");
+
+            if (randomWord == underLine)
+            {
+                Console.WriteLine("congrats! You win");
+                break;
+            }           
+            else { }
+        }    
+    }
+}
