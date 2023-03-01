8691d6415c7dd181bb52b9497f68b53db9aca73f
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-01 21:24:15 +0200

    Used loop to count how many times rolled anything but 1. And checked if you wanted to play again y or n?

---
+++ csharp-basics/exercises/Loops/Loops/Exercise_7/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Piglet
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to piglet");
+        int counter = 0;
+        int loopCounter = 1;
+        Random rnd = new Random();
+
+        for (int i = 0; i < loopCounter; i++) 
+        {
+            if (rnd.Next(1,7)  != 1)
+            {
+                counter++;
+                loopCounter++;
+            }
+            else
+            {
+                Console.WriteLine($"you got the score {counter}");
+                Console.WriteLine("wanna play again y or n?"); ;
+                if (Console.ReadKey().KeyChar == 'y')
+                {
+                    loopCounter++;
+                    counter = 0;
+                }
+                else 
+                {
+                    break;
+                }               
+            }
+        }       
+
+    }
+}
