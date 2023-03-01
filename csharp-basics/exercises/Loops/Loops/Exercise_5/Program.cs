16fee7465ebc59f3be2a998b242d1f700cc7609b
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-01 21:06:15 +0200

    Used loop just to count dots between words.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Channels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("insert first word:");
+        string firstWord = Console.ReadLine();
+        Console.WriteLine("insert second word:");
+        string secondWord = Console.ReadLine();
+        int firsLength = firstWord.Length;
+        int secondLength = secondWord.Length;
+        char[] dots = new char[30 - firsLength-secondLength];
+        string dotsCount = "";
+
+        for(int i = 0; i < dots.Length; i++)
+        {
+            dotsCount += '.';
+        }
+
+        Console.WriteLine($"{firstWord}{dotsCount}{secondWord}");
+    }
+}
