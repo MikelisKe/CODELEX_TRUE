9f6da65c1db85901ec596b3f3f10644371eb6479
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-02 00:46:03 +0200

    Rolled the dice with 2 rand numbers sum in the loop and if statements

---
+++ csharp-basics/exercises/Loops/Loops/Exercise_9/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Desired sum = 9");
+        Random rnd = new Random();
+        int desired = 9;
+        int counter = 1;
+        int iterations = 0;
+
+        for(int i = 0; i < counter; i++) 
+        {
+            int x = rnd.Next(1,7);
+            int y = rnd.Next(0,7);
+            int z = x + y;
+            if(x + y != 9)
+            {
+                Console.WriteLine($"{x} and {y} = {z}");
+                counter++;
+                iterations++;
+            }
+            else
+            {
+                Console.WriteLine($"{x} and {y} = {z}");
+                Console.WriteLine($"Yougot lucky after {iterations} times ");
+            }
+        }
+        Console.ReadKey();
+    }
+}
