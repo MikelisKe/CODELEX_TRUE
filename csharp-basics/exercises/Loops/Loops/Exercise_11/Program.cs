1be1b0ae496195d774c811270ac9951606b7650f
Miķelis Keišs <mikelis.keiss90@gmail.com>
2023-03-02 10:54:45 +0200

    String to char, then loop, if upper => lower and contrariwise

---
+++ csharp-basics/exercises/Loops/Loops/Exercise_11/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ReverseTheCase("ZirgA AsTiTe");
+        static string ReverseTheCase(string input)
+        {
+            string result = "";
+            char[] chars = input.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == char.ToUpper(chars[i]))
+                {
+                    chars[i] =  char.ToLower(chars[i]);
+                }
+                else
+                {
+                    chars[i] = char.ToUpper(chars[i]);
+                }
+                result = result + chars[i];
+            }
+
+
+            return result;
+        }
+    }
+}
