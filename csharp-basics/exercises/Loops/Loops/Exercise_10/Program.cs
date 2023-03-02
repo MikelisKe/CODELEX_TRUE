c9d31cb651b5a5142a89318001da037835a0df8f
Miķelis Keišs <mikelis.keiss90@gmail.com>
2023-03-02 09:38:27 +0200

    Used nested for loops to get the result.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise_10/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class NumberSquare
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Min?");
+        int min = int.Parse(Console.ReadLine());
+        Console.WriteLine("Max?");
+        int max = int.Parse(Console.ReadLine());
+
+        for (int i = min; i <= max; i++)
+        {
+            for (int iter = i; iter <= max; iter++)
+            {
+                Console.Write(iter);
+            }
+            for (int a = min; a < i; a++)
+            {
+                Console.Write(a);
+            }
+
+            Console.WriteLine();        
+        }
+    }
+}
