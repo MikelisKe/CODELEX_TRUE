d11791b657976e3ac612d172dbc49b6523f3c965
Miķelis Keišs <mikelis.keiss90@gmail.com>
2023-03-03 14:41:00 +0200

    Its just a start.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_1/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Product
+    {
+        public double price;
+        public double ammount;
+        private string name;
+
+        public Product(string aname, double priceAtStart, int amountAtStart)
+        {
+            name = aname;
+            price = priceAtStart; 
+            ammount = amountAtStart;
+            Console.WriteLine($"{name}, priece {price}, amount {ammount}");
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Exercise_1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Product product1 = new Product("Banana", 1.1, 13);
+        Product product2 = new Product("Logitech mouse", 70.00, 14);
+        Product product3 = new Product("iPhone 5s", 999.99, 3);
+        Product product4 = new Product("Epson EB-U05", 440.46, 1);
+
+        Console.WriteLine(product1);
+        Console.WriteLine(product2);
+        Console.WriteLine(product3);
+        Console.WriteLine(product4);
+        Console.ReadKey();
+    }
+}
