c99553d1b48f260fbf61550243f7287901616f36
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-05 23:17:04 +0200

    Used constructor to create products, made name private, and methods to change price or quantity.

--- csharp-basics/exercises/ClassesAndObjects/Exercise_1/Product.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_1/Product.cs
@@ -2,22 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Exercise_1
 {
     class Product
     {
-        public double price;
-        public double ammount;
+        public double Price;
+        public int ammount;
         private string name;
 
-        public Product(string aname, double priceAtStart, int amountAtStart)
+        public Product(string aName, double aPrice, int aAmmount)
         {
-            name = aname;
-            price = priceAtStart; 
-            ammount = amountAtStart;
-            Console.WriteLine($"{name}, priece {price}, amount {ammount}");
+            name = aName;
+            Price = aPrice;
+            ammount = aAmmount;
+        }
+
+        public string PrintProduct()
+        {
+            string b = name + ", Price " + Price.ToString() + ", ammount "+ammount.ToString();
+            return b;
+        }
+
+        public int ChangeQuantitity()
+        {
+            Console.WriteLine("Whats the new ammount of product, you want to add");
+            int q = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            return ammount + q;
+        }
+        public double ChangePrice()
+        {
+            Console.WriteLine("Whats the new price?");
+            double p = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            return p;
         }
     }
 }
+
--- csharp-basics/exercises/ClassesAndObjects/Exercise_1/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_1/Program.cs
@@ -1,18 +1,21 @@
-﻿using Exercise_1;
+﻿using System.Threading.Channels;
+using Exercise_1;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Product product1 = new Product("Banana", 1.1, 13);
-        Product product2 = new Product("Logitech mouse", 70.00, 14);
-        Product product3 = new Product("iPhone 5s", 999.99, 3);
-        Product product4 = new Product("Epson EB-U05", 440.46, 1);
+        Product product1 = new Product("Logitech mouse", 70.00, 14);
+        Product product2 = new Product("iPhone 5s", 999.99, 3);
+        Product product3 = new Product("Epson EB-U05", 440.46, 1);
+        
+        Console.WriteLine(product1.PrintProduct());
+        Console.WriteLine(product2.PrintProduct());
+        Console.WriteLine(product3.PrintProduct());
 
-        Console.WriteLine(product1);
-        Console.WriteLine(product2);
-        Console.WriteLine(product3);
-        Console.WriteLine(product4);
+        Console.WriteLine(product3.ChangePrice());
+        Console.WriteLine(product3.ChangeQuantitity());
+
         Console.ReadKey();
     }
 }
