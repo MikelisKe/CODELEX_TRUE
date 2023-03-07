e6a8d10c56eaea66ac9b15731f04e48c48c98778
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-07 13:33:25 +0200

    Created Movie Class, with additional get set values and in List with foreach looped through the list for PG and added it into PGmovie array.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_4/Movie.cs
@@ -0,0 +1,60 @@
+﻿namespace Exercise_4;
+
+public class Movie
+{
+    private string title;
+    private string studio;
+    private string rating;
+    public override string ToString()
+    {
+        return $"{title}, {studio}, {rating}";
+
+    }
+
+    public Movie(string _title, string _studio, string _rating)
+    {
+        title = _title;
+        studio = _studio;
+        Rating = _rating;
+    }
+
+    public Movie(string _title, string _studio) : this(_title,_studio, "PG")
+    {
+        
+    }
+    public string Rating
+    {
+        get => rating;
+        set
+        {
+            if (value == "PG" || value == "PG-13")
+            {
+                rating = value;
+            }
+            else
+            {
+                rating = "NR";
+            }
+        }
+    }
+
+    public static Movie[] GetPG(Movie[] movies)
+    {
+        List<Movie> pgMovies = new List<Movie>();
+        foreach (Movie movie in movies)
+        {
+            if (movie.Rating == "PG")
+            {
+                pgMovies.Add(movie);
+            }
+        }
+        return pgMovies.ToArray();
+    }
+
+    /*public void showMovies()
+    {
+        
+    }*/
+
+
+}
