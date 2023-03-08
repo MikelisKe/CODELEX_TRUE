4454faeafe61d9c9f12ee5bdc3fda33454b1875c
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-08 14:52:52 +0200

    Created SwapPoint and parameters as ref.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_8/SwapPoints .cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace Exercise_8;
+
+public class SwapPoints
+{
+    private int _x;
+    private int _y;
+
+    public SwapPoints (int x, int y)
+    {
+        _x = x; 
+        _y = y;
+    }
+}
