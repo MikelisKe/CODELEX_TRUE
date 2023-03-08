adf0a1dfcf3fd9b655d86c55c8641d39241fd554
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-08 23:54:07 +0200

    Used datetime and cultureinfo as instanse. Input the date. My kids bday.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_14/Date.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using Microsoft.VisualBasic;
+
+namespace Exercise_14;
+
+public class Date
+{
+    private int _year;
+    private int _month;
+    private int _day;
+
+    public Date(int year, int month, int day)
+    {
+        _year = year;
+        _month = month; 
+        _day = day;
+    }
+
+    public string Day(){
+
+        DateTime dateValue = new DateTime(_year, _month, _day);
+        CultureInfo culture = new CultureInfo("nl-NL");
+        return culture.DateTimeFormat.GetDayName(dateValue.DayOfWeek);
+    }
+}
