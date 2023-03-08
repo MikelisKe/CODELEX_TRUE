2c6d5d5eec19591ac082c60c1f0ec5c80c286595
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-08 13:09:19 +0200

    Ex 7 done. Did it without loop. Guess thats fine untill program works.

--- csharp-basics/exercises/ClassesAndObjects/Exercise_7/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_7/Program.cs
@@ -1,13 +1,11 @@
 ﻿using Exercise_7;
 
 
-SavingsAccount account = new SavingsAccount(10000,0,0);
-int annualRate = 5;
-int accOpened = 4;
+SavingsAccount account = new SavingsAccount(10000, 0, 0, 0, 0);
 
 Console.WriteLine($"How much money is in the account?: {account.GetBalance()}");
-Console.WriteLine($"Enter the annual interest rate:{annualRate}");
-Console.WriteLine($"How long has the account been opened? {accOpened}");
+Console.WriteLine($"Enter the annual interest rate:{account.annualRate}");
+Console.WriteLine($"How long has the account been opened? {account.accOpened}");
 Console.WriteLine($"Enter amount deposited for month: 1: {account.AddingDeposit(100)}");
 Console.WriteLine($"Enter amount withdrawn for 1: {account.Withdrawal(1000)}");
 Console.WriteLine($"Enter amount deposited for month: 2: {account.AddingDeposit(230)}");
@@ -17,10 +15,10 @@ Console.WriteLine($"Enter amount withdrawn for 3: {account.Withdrawal(2334)}");
 Console.WriteLine($"Enter amount deposited for month: 4: {account.AddingDeposit(3450)}");
 Console.WriteLine($"Enter amount withdrawn for 4: {account.Withdrawal(2340)}");
 Console.WriteLine();
-Console.WriteLine($"Total deposited: ${account.AmmoundAdded().ToString("F2")}");
-Console.WriteLine($"Total withdrawn: ${account.AmmoundRemoved().ToString("F2")}");
-Console.WriteLine($"Interest earned: $29,977.72");
-Console.WriteLine($"Ending balance: ${account.GetBalance().ToString("F2")}");
+Console.WriteLine($"Total deposited: ${account.AmmoundAdded():F2}");
+Console.WriteLine($"Total withdrawn: ${account.AmmoundRemoved():F2}");
+Console.WriteLine($"Interest earned: ${account.ReportInterest():F2}");
+Console.WriteLine($"Ending balance: ${account.GetBalance():F2}");
 
 
 
--- csharp-basics/exercises/ClassesAndObjects/Exercise_7/SavingsAccount.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise_7/SavingsAccount.cs
@@ -4,38 +4,55 @@ namespace Exercise_7;
 
 public class SavingsAccount
 {
-    private decimal _balance;
+    private decimal _bilance;
     private decimal _amountAdd;
     private decimal _amountRemove;
+    private decimal _interestCharged;
+    private decimal _reportInterest; 
+    public decimal annualRate = 5;
+    public decimal accOpened = 4;
 
-    public SavingsAccount(decimal balance, decimal amountAdd, decimal amountRemove)
+    public SavingsAccount(decimal bilance, decimal amountAdd, decimal amountRemove, decimal interestCharged, decimal reportInterest)
     {
-        _balance = balance;
+        _bilance = bilance;
         _amountAdd = amountAdd;
         _amountRemove = amountRemove;
+        _interestCharged = interestCharged;
+        _reportInterest = reportInterest;
     }
 
+    public decimal Interest(decimal amount)
+    {
+        decimal monthlyRate = annualRate/12;
+        decimal interest = monthlyRate * _bilance;
+        _reportInterest += interest;
+        _interestCharged = interest;
+
+        return interest;
+    }
     public decimal GetBalance()
     {
-        return _balance;
+        return _bilance;
     }
 
     public decimal AddingDeposit (decimal amount)
     {
         _amountAdd += amount;
-        _balance += amount;
-       return amount;
+        _bilance += amount;
+        return amount;
     }
     public decimal Withdrawal(decimal amount)
     {
-        if (amount > _balance)
+        if (amount > _bilance)
         {
             return 0;
         }
         else
         {
             _amountRemove += amount;
-            _balance -= amount;
+            _bilance -= amount;
+            Interest(_bilance);
+            _bilance += _interestCharged;
             return amount;
         }
     }
@@ -48,5 +65,15 @@ public class SavingsAccount
     public decimal AmmoundRemoved()
     {
         return _amountRemove;
+    }
+
+    public decimal AmmoundInterest()
+    {
+        return _interestCharged;
+    }
+
+    public decimal ReportInterest()
+    {
+        return _reportInterest;
     }
 }
