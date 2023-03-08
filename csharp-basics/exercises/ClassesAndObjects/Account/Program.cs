3f7c658d258e7798813c6e87aa4343c08a4d7984
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-08 20:10:42 +0200

    Test

--- csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
@@ -10,6 +10,40 @@ namespace Account
     {
         private static void Main(string[] args)
         {
+            /*Account bartosAccount = new Account("Barto's account", 100.00);
+            Account bartosSwissAccount = new Account("Barto's account in Switzerland", 1000000.00);
+            Console.WriteLine("Initial state");
+            Console.WriteLine(bartosAccount);
+            Console.WriteLine(bartosSwissAccount);
+            bartosAccount.Withdrawal(20);
+            Console.WriteLine("Barto's account balance is now: " + bartosAccount.Balance());
+            bartosSwissAccount.Deposit(200);
+            Console.WriteLine("Barto's Swiss account balance is now: " + bartosSwissAccount.Balance());
+            Console.WriteLine("Final state");
+            Console.WriteLine(bartosAccount);
+            Console.WriteLine(bartosSwissAccount);*/
+
+            string FirstAccount()
+            {
+                Account first = new Account("first",100.0);
+                first.Withdrawal(20);
+                return first.ToString();
+            }
+
+            string MoneyTransfer()
+            {
+                Account matt = new Account("Matt's account", 1000);
+                Account my = new Account("My account", 0);
+                var deal = 100;
+                matt.Withdrawal(deal);
+                my.Deposit(deal);
+                return $"{matt.ToString()} \n{my.ToString()}";
+            }
+
+            Console.WriteLine(MoneyTransfer());
+            ;
+            //Console.WriteLine(FirstAccount());
+            Console.ReadKey();
         }
     }
 }
