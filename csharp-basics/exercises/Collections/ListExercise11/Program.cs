4d702863595af62c53f9435750c45de717cb2d54
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-12 12:59:56 +0200

    Created Dictionarry and witf Select got all the values i needed. For other exercises, cleaned up some.

--- csharp-basics/exercises/Collections/ListExercise11/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise11/Program.cs
@@ -10,10 +10,8 @@ namespace ListExercise11
     {
         static void Main(string[] args)
         {
-            //TODO: Create an List with string elements
             List<string> list = new List<string>();
 
-            //TODO: Add 10 values to list
             list.Add("a");
             list.Add("c");
             list.Add("b");
@@ -28,12 +26,10 @@ namespace ListExercise11
             Console.WriteLine("list of 10 elements:");
             Console.WriteLine(string.Join(",", list));
 
-            //TODO: Add new value at 5th position
             list.Insert(4, "l");
             Console.WriteLine("Inserted nev value in 5th position:");
             Console.WriteLine(string.Join(",", list));
 
-            //TODO: Change value at last position (Calculate last position programmatically)
             var a = list.Count -1;
             list[a] = "k";
 
@@ -41,18 +37,15 @@ namespace ListExercise11
             Console.WriteLine(string.Join(",", list));
 
 
-            //TODO: Sort your list in alphabetical order
             list.Sort();
             Console.WriteLine("Alphabetical order ");
             Console.WriteLine(string.Join(",", list));
 
 
-            //TODO: Check if your list contains "Foobar" element
 
             var foo = list.Contains("Foobar") ? true :false;
             Console.WriteLine($"Does list contains foobar?, its {foo}");
 
-            //TODO: Print each element of list using loop
 
             Console.WriteLine("printing out using for loop:");
             for (int i = 0; i < list.Count; i++)
