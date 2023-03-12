e20c75299f8c075a951fe2852cf9957a00a75387
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-12 15:06:50 +0200

    Used While string.IsNullOrEmpty to check if the value is entered, then checked for unique values with distinct

---
+++ csharp-basics/exercises/Collections/Exercise_4/Program.cs
@@ -0,0 +1,16 @@
+﻿
+List<string> result = new List<string>();
+
+Console.WriteLine("Write people names, then return");
+var name = Console.ReadLine();
+
+while (!string.IsNullOrEmpty(name))
+{
+    result.Add(name);
+    name = Console.ReadLine();
+}
+
+result = result.Distinct().ToList();
+
+Console.WriteLine("Unique names:");
+Console.WriteLine(string.Join(",", result));
