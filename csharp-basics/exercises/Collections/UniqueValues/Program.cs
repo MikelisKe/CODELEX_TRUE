575b49f83a77921d79460e7bf39d876df18818b7
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-12 14:41:16 +0200

    Used distinct to output uniquie values

--- csharp-basics/exercises/Collections/UniqueValues/Program.cs
+++ csharp-basics/exercises/Collections/UniqueValues/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
+
 
 namespace UniqueValues
 {
@@ -7,11 +10,12 @@ namespace UniqueValues
     {
         static void Main(string[] args)
         {
-            //ToDo: Given a non-empty list of strings, return a list that contains only unique (non-duplicate) strings.
-            //ToDo: Example: ["abc", "xyz", "klm", "xyz", "abc", "abc", "rst"] → ["klm", "rst"]
-
             var values = new List<string> { "Hi", "Meow", "Hello", "Meow", "Hi!", "Meow", "Hi", "Bye" };
 
+            var result = values.Distinct().ToList();
+
+            Console.WriteLine(String.Join(",", result));
+            Console.ReadKey();
         }
     }
 }
