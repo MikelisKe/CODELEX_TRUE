f936b0156e71d3273f679fd7665d80a95a32e5ba
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-12 23:02:22 +0200

    Used read all text, read allLines.count. Splitted with ' ', ''' ? and !. than length Then counted all the chars - !?',

--- csharp-basics/exercises/Collections/WordCount/Program.cs
+++ csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WordCount
@@ -10,7 +12,22 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var text = System.IO.File.ReadAllText(@"C:\Users\Mikelis\Desktop\00_Darba maiņa\Codelex\CODELEX_LOCAL\csharp-basics\exercises\Collections\WordCount\lear.txt");
+            Console.WriteLine(text);
+            var lineCount = File.ReadAllLines(@"C:\Users\Mikelis\Desktop\00_Darba maiņa\Codelex\CODELEX_LOCAL\csharp-basics\exercises\Collections\WordCount\lear.txt").Length;
+            Console.WriteLine(lineCount);
+            var wordCount = text.Split(' ', '\t',',','?','!').Length;
+            Console.WriteLine(wordCount);
+            var symbolCount = text.Count() - 
+                              text.Count(f => f == '!') - 
+                              text.Count(f => f == '?') -
+                              text.Count(f => f == '\t') - 
+                              text.Count(f => f == ',');
+
+            Console.WriteLine(symbolCount);
+
+            Console.ReadLine();
+
         }
     }
 }
