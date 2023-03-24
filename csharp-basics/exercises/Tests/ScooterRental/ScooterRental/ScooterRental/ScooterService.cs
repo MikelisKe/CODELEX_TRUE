5128947da4e5ef3743b1b7260280498ce52a6657
Miķelis Keišs <mikelis.keiss90@gmail.com>
2023-03-24 15:33:16 +0200

    Testu sakums

---
+++ csharp-basics/exercises/Tests/ScooterRental/ScooterRental/ScooterRental/ScooterService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScooterRental
+{
+    internal class ScooterService : IScooterService
+    {
+        List<>
+        public void AddScooter(string id, decimal pricePerMinute)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveScooter(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Scooter> GetScooters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Scooter GetScooterById(string scooterId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
