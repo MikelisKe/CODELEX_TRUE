ffbc59b6ce6ea07d55fa06c9ade3b26b4031f83c
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2023-03-27 08:53:04 +0300

    Kautkas uz priekšu

---
+++ csharp-basics/exercises/Polymorphism/Persons.test/CatTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persons
+{
+    public class CatTests
+    {
+        private Cat _cat;
+
+        public void Setup()
+        {
+            _cat = new Cat("name", 1.4, 0, "purchix", "Staltais");
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+
+
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Persons.test/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace Persons.test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestAnimalMakeSoundOutput()
+        {
+            TestContext.Out.WriteLine("First_test");
+
+        }
+    }
+}
