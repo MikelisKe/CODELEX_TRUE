b49e962408ff010008a043caec6d19fd6dbc1b4e
Miķelis Keišs <mikelis.keiss90@gmail.com>
2023-04-17 15:40:01 +0300

    Kautnas

--- csharp-basics/exercises/Tests/ScooterRental/ScooterRental/ScooterRental.test/RentalCompanyTest.cs
+++ csharp-basics/exercises/Tests/ScooterRental/ScooterRental/ScooterRental.test/RentalCompanyTest.cs
@@ -9,36 +9,32 @@ public class RentalCompanyTest
 {
     private AutoMocker _mocker;
     private IRentalCompany _company;
-    private const string CompanyName = "test";
-    private Scooter.Scooter scooter = new Scooter.Scooter("1", 0.00m);
+    private const string _companyName = "test";
+    private Scooter.Scooter _scooter = new Scooter.Scooter("1", 0.00m);
 
     [SetUp]
     public void Setup()
     {
         _mocker = new AutoMocker();
-        _company = new RentalCompany(CompanyName, _mocker.GetMock<IScooterService>().Object);
+        _company = new RentalCompany(_companyName, _mocker.GetMock<IScooterService>().Object);
     }
 
     [Test]
     public void StartRent_ScooterIsRented_ReturnsTrue()
     {
-        _mocker.GetMock<IScooterService>().Setup(b => b.GetScooterById("1")).Returns(scooter);
+        _mocker.GetMock<IScooterService>().Setup(b => b.GetScooterById("1")).Returns(_scooter);
 
         _company.StartRent("1");
-        scooter.IsRented.Should().BeTrue();
+        _scooter.IsRented.Should().BeTrue();
     }
 
     [Test]
     public void StartRent_ScooterRentHasEnded_ReturnsFalse()
     {
-        scooter.IsRented = true;
-        _mocker.GetMock<IScooterService>().Setup(b => b.GetScooterById("1")).Returns(scooter);
+        _scooter.IsRented = true;
+        _mocker.GetMock<IScooterService>().Setup(b => b.GetScooterById("1")).Returns(_scooter);
 
         _company.EndRent("1");
-        scooter.IsRented.Should().BeFalse();
+        _scooter.IsRented.Should().BeFalse();
     }
-
-
-
-
 }
--- csharp-basics/exercises/Tests/ScooterRental/ScooterRental/ScooterRental/RentalCalculator.cs
+++ csharp-basics/exercises/Tests/ScooterRental/ScooterRental/ScooterRental/RentalCalculator.cs
@@ -3,22 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ScooterRental.Interfaces;
 
 namespace ScooterRental
 {
     internal class RentalCalculator
     {
-        /*need DateTime Startrent
-         *need DateTime EndRent
-         * Need RentPrice
-         */
-        
 
-        public RentalCalculator()
-        {
-
-        }
-
-        public void CalculateFee(RentedScooter )
+        public void CalculateFee(IRentalCompany 
     }
 }
