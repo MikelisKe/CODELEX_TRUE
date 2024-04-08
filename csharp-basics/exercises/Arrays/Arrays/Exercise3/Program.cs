c2a0d9cfffdccf8f10c010127a8e45b71e27c4a1
Mikelis <Mikelis@DESKTOP-0T0G5EU>
2024-04-08 20:53:50 +0300

    Uzd 1 papildināts.

--- csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercise3
 {
@@ -6,15 +8,26 @@ namespace Exercise3
     {
         private static void Main(string[] args)
         {
-            int[] numbers = {20, 30, 25, 35, -16, 60, -100};
+            // int[] numbers = {20, 30, 25, 35, -16, 60, -100};
             int sum = 0;
+            var rnd = new Random(10);
 
-            for (int i = 0 ; i< numbers.Length; i++)
+            List<int> numbers = new List<int>();
+            int randomeTimes = rnd.Next(1,10);
+            Console.WriteLine($"Enter {randomeTimes} random numbers to measure their average number: ");
+
+            for (int i = 0; i < randomeTimes; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                numbers.Add(num);
+            };
+
+            for (int i = 0 ; i< numbers.Count; i++)
             {
                 sum += numbers[i];
             }
 
-            int average = sum / (numbers.Length);                     
+            int average = sum / (numbers.Count);                     
             Console.WriteLine("Average value of the array elements is : " + average);
             Console.ReadKey();            
         }
